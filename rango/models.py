b5ee1262e128a8bdd83b057dfbe7b45dcb62d601
Fiona Johnson <9905602j@student.gla.ac.uk>
2019-02-06 16:36:27 +0000

    chapter 6 exercises complete and working

--- rango/models.py
+++ rango/models.py
@@ -9,7 +9,7 @@ class Category(models.Model):
     
     def save(self, *args, **kwargs):
         self.slug = slugify(self.name)
-        super(Category, self).save(*args, *kwargs)
+        super(Category, self).save(*args, **kwargs)
     
     class Meta:
         verbose_name_plural = 'categories'
