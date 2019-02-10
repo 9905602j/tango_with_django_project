640b0ffec2c0567c4f6ac28c1fffc91ca2a74dfd
Fiona Johnson <9905602j@student.gla.ac.uk>
2019-02-10 12:54:55 +0000

    Chapter 9 exercises complete

--- rango/views.py
+++ rango/views.py
@@ -36,6 +36,7 @@ def show_category(request, category_name_slug):
         context_dict['pages'] = None
     return render(request, 'rango/category.html', context_dict)
 
+@login_required
 def add_category(request):
     form = CategoryForm()
     
@@ -49,6 +50,7 @@ def add_category(request):
             print(form.errors)
     return render(request, 'rango/add_category.html', {'form': form})
 
+@login_required
 def add_page(request, category_name_slug):
     try:
         category = Category.objects.get(slug=category_name_slug)
@@ -114,13 +116,13 @@ def user_login(request):
                 return HttpResponse("Your Rango account is disabled.")
         else:
             print("Invalid login details: {0}, {1}".format(username, password))
-            return HttpResponse("Invalid login details supplied.")
+            return render(request, 'Rango/login.html', {"error":"You got your username or password wrong"})
     else:
         return render(request, 'Rango/login.html', {})
     
 @login_required
 def restricted(request):
-    return HttpResponse("Since you're logged in, you can see this text!")
+    return render(request, 'Rango/restricted.html', {})
 
 @login_required
 def user_logout(request):
