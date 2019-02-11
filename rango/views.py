4988e05a46ff1ef89dafc15c079606dfbac55eba
Fiona Johnson <9905602j@student.gla.ac.uk>
2019-02-11 18:08:26 +0000

    chapter 10 exercises complete

--- rango/views.py
+++ rango/views.py
@@ -31,7 +31,11 @@ def about(request):
     if request.session.test_cookie_worked():
         print("TEST COOKIE WORKED!")
         request.session.delete_test_cookie()
-    return render(request, 'rango/about.html')
+    context_dict = {}
+    visitor_cookie_handler(request)
+    context_dict['visits'] = request.session['visits']
+    response = render(request, 'rango/about.html', context=context_dict)
+    return response
 
 def show_category(request, category_name_slug):
     context_dict = {}
